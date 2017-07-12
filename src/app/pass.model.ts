ae3e7f48ca1be5fba93314a095848748177a6cd2
Caitlin Hines <caitlinhines@me.com>
2017-07-12 15:41:35 -0700

    displaying ticket info with pass route

---
+++ src/app/pass.model.ts
@@ -0,0 +1,3 @@
+export class Pass {
+  constructor (public title: string, public description: string, public price: number, public id: number) { }
+}
--- src/app/passes/passes.component.ts
+++ src/app/passes/passes.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Pass } from '../pass.model'
 
 @Component({
   selector: 'app-passes',
   templateUrl: './passes.component.html',
   styleUrls: ['./passes.component.css']
 })
-export class PassesComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class PassesComponent {
+  passes: Pass[] = [
+  new Pass("Daily Lift ticket", "Skiing from 9am-4pm", 65, 1),
+  new Pass("Advantage Ticket", "Skiing 4 times from 9am-4pm", 150, 2),
+  new Pass("College pass", "Skiing from 9am-4pm all season for college students", 300, 3),
+  new Pass("Season's Pass", "Skiing from 9am-4pm all season", 500, 4),
+ ];
 }
